445367dba0d764b732f9b2960f5d67a67a4edc45
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-05-20 01:07:23 +0530

    Add custom env variables

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '.env.local' });
 
-const db = new Sequelize('ayushidb', 'cms-user', 'AG16#mysql', {
-  host: 'localhost',
-  dialect: 'mysql',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
 });
 
 module.exports = db;
